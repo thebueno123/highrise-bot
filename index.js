b9b7a9256b4b034cbeed74a35bc171eb43397acd
thebueno123 <manuelbueno14@yahoo.com>
2025-07-04 20:12:20 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+import WebSocket from "ws";
+import dotenv from "dotenv";
+dotenv.config();
+
+const ws = new WebSocket("wss://gateway.highrise.game/web/web-socket");
+
+ws.on("open", () => {
+  console.log("✅ Connected to Highrise gateway");
+
+  ws.send(
+    JSON.stringify({
+      _type: "AuthenticateRequest",
+      token: process.env.API_KEY,
+    })
+  );
+});
+
+ws.on("message", (data) => {
+  const message = JSON.parse(data);
+  console.log("📨", message);
+
+  if (
+    message._type === "UserJoinedEvent" &&
+    message.user &&
+    message.user.username
+  ) {
+    const username = message.user.username;
+
+    ws.send(
+      JSON.stringify({
+        _type: "SendMessageRequest",
+        message: `👋 Welcome in @${username}! 🎁 1 item per person | 💺 Sit in line | 💸 Tips appreciated!`,
+        roomId: process.env.ROOM_ID,
+
+      })
+    );
+  }
+});
